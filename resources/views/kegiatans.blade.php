fe1247d371a7fbc4edfa19117a109c37492dca1c
Farry-Rangian <farry12angian@gmail.com>
2021-11-03 10:01:31 +0800

    merubah tampilan pada kegiatans.blade

--- resources/views/kegiatans.blade.php
+++ resources/views/kegiatans.blade.php
@@ -1,19 +1,47 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="card mb-3">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+@if ($kegiatans->count())
+  <div class="card mb-3">
+    <img src="https://source.unsplash.com/1200x400/?nature,water" class="card-img-top" alt="...">
+    <div class="card-body text-center">
+      <h3 class="card-title">{{ $kegiatans[0]->name }}</h3>
+        <p class="card-text">
+          <small class="text-muted">
+            By : {{ $kegiatans[0]->admin->name }}
+            {{ $kegiatans[0]->created_at->diffForHumans()}}
+          </small>
+        </p>
+      <p class="card-text">{{ $kegiatans[0]->content }}</p>
+      <a href="/kegiatans/{{ $kegiatans[0]->id }}" class="text-decoration-none btn btn-primary">Read more</a>
+    </div>
+  </div>
+@else
+  <p class="text-center fs-4">No post Found</p>
+@endif
+
+<div class="container">
+  <div class="row">
+    @foreach ($kegiatans->skip(1) as $kegiatan)
+    <div class="col-md-4 mb-3">
+      <div class="card">
+        <img src="https://source.unsplash.com/500x400/?nature" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">
+            <a href="/kegiatans/{{ $kegiatan->id }}">{{ $kegiatan->name }}</a>
+          </h5>
+          <p class="card-text">
+            <small class="text-muted">
+              By : {{ $kegiatan->admin->name }}
+              {{ $kegiatan->created_at->diffForHumans()}}
+            </small>
+          </p>
+          <p>{{ $kegiatan->content }}</p>
+          <a href="/kegiatans/{{ $kegiatan->id }}" class="btn btn-primary">Read more</a>
+        </div>
+      </div>
+    </div>
+    @endforeach
   </div>
 </div>
-  @foreach ($kegiatans as $kegiatan)
-  <article class="mb-5">
-    <h2>
-      <a href="/kegiatans/{{ $kegiatan->id }}">{{ $kegiatan->name }}</a>
-    </h2>
-    <p>{{ $kegiatan->content }}</p>
-  @endforeach
 @endsection
