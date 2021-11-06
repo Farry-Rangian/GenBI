0bb7f3aa427b6e06a46827e38e69c226f704b6c3
Farry-Rangian <farry12angian@gmail.com>
2021-11-06 19:20:41 +0800

    membuat halaman dashboard

--- app/Models/Artikel.php
+++ app/Models/Artikel.php
@@ -15,4 +15,9 @@ class Artikel extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
---
+++ resources/views/dashboard/artikels/show.blade.php
@@ -0,0 +1,22 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+<div class="container">
+    <div class="row my-3">
+        <div class="col-lg-8">
+            <article>
+                <h2>{{ $artikel->judul }}</h2>
+                <img src="https://source.unsplash.com/1200x600/?nature" alt="" class="img-fluid mt-3">
+                <article class="my-3 fs-5">
+                    {!! $artikel->content !!}
+                </article>
+                
+            </article>
+            <br>
+            <a href="/dashboard/artikels" class="btn btn-success"><span data-feather="arrow-left"></span> Back to all my articles</a>
+            <a href="" class="btn btn-warning"><span data-feather="edit"></span> Edit</a>
+            <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
+        </div>
+    </div>
+</div>
+@endsection
