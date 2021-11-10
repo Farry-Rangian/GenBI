09f39155f9e28a9b79f0cd25186dd2399deb1921
Farry-Rangian <farry12angian@gmail.com>
2021-11-10 10:18:00 +0800

    ui ujian belum fix

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -13,6 +13,8 @@
     
     <!-- Custom styles for this template -->
     <link href="/css/dashboard.css" rel="stylesheet">
+
+    <link href="/css/style1.css" rel="stylesheet">
   </head>
   <body>
     
--- resources/views/struktur.blade.php
+++ resources/views/struktur.blade.php
@@ -1,5 +1,33 @@
 @extends('layouts.main')
 
 @section('container')
-  <h1>Halaman Struktur Kepengurusan</h1>
+  <h1>Halaman Ujian</h1>
+  <div>
+    <div class="content">
+        <div class="card">
+            <h2>Pilih ujian yang ingin diikuti</h2>
+            <div class="head-card">
+                <a href="soal-1.html"><img src="/img/ujian.png" alt="ujian-1"></a>
+
+            </div>
+
+            <div class="body-card">
+                <a href="soal-1.html">
+                    <h1>Ujian Kelayakan</h1>
+                    <h4>waktu : 10-11-2021</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad repudiandae fuga facilis
+                        dicta
+                        fugiat voluptates deleniti libero earum sed aliquid. </p>
+                    <svg class="w-6 h-6" fill="none" stroke="#303030" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M17 8l4 4m0 0l-4 4m4-4H3"></path>
+                    </svg>
+                </a>
+
+            </div>
+        </div>
+
+    </div>
+</div>
 @endsection
